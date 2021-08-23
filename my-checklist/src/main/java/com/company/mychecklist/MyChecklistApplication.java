7ad8fd47aadfdd5980af9386430ae4f56214c872
Thiago Cunha <thiagoferrax@gmail.com>
2021-08-23 17:53:11 -0300

    Creating my-checklist project using Spring Starter Project with Spring Web, JPA and H2 Databse.

---
+++ my-checklist/src/main/java/com/company/mychecklist/MyChecklistApplication.java
@@ -0,0 +1,13 @@
+package com.company.mychecklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChecklistApplication.class, args);
+	}
+
+}
---
+++ my-checklist/src/test/java/com/company/mychecklist/MyChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.mychecklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
