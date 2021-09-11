07a0d62503efdbc6095310756b6d90df0af84b2f
Thiago Cunha <thiagoferrax@gmail.com>
2021-09-11 18:26:42 -0300

    Adding dependencies and removing dist folder.

--- admin-my-checklist/src/Menu.js
+++ admin-my-checklist/src/Menu.js
@@ -7,16 +7,16 @@ export default class Menu extends Component {
                 <aside className="main-sidebar sidebar-dark-primary elevation-4">
                     {/* Brand Logo */}
                     <a href="index3.html" className="brand-link">
-                        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
+                        {/* <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />*/}
                         <span className="brand-text font-weight-light">My Checklist</span>
                     </a>
                     {/* Sidebar */}
                     <div className="sidebar">
                         {/* Sidebar user panel (optional) */}
                         <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-                            <div className="image">
+                            {/* <div className="image">
                                 <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
-                            </div>
+                            </div>*/}
                             <div className="info">
                                 <a href="#" className="d-block">Thiago Ferraz</a>
                             </div>
